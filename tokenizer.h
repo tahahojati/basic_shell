44b45b1f64892cc611b2beb6cc43c70dcbb35c2a
Taha Pourjalali <taha.hojati@gmail.com>
2018-02-20 19:23:22 +0000

    initial commit

---
+++ tokenizer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* A struct that represents a list of words. */
+struct tokens;
+
+/* Turn a string into a list of words. */
+struct tokens *tokenize(const char *line);
+
+/* How many words are there? */
+size_t tokens_get_length(struct tokens *tokens);
+
+/* Get me the Nth word (zero-indexed) */
+char *tokens_get_token(struct tokens *tokens, size_t n);
+
+/* Free the memory */
+void tokens_destroy(struct tokens *tokens);
+static void *vector_push(char ***pointer, size_t *size, void *elem);
